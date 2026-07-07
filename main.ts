df42ed880507d7a2b75e5011624d7337f496ac23
Moonsung Kim <jasonkim2004@gmail.com>
2026-07-07 12:12:27 -0400

    [07/07] Step #3 - Save coordinates to keep previous location

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
 import { Plugin } from "obsidian";
 
+interface ObsidianChiikawaSettings {
+  boxX: number;
+  boxY: number;
+}
+
+const DEFAULT_SETTINGS: ObsidianChiikawaSettings = {
+  boxX: 160,
+  boxY: 160,
+};
+
 export default class ObsidianChiikawaPlugin extends Plugin {
   private boxEl: HTMLDivElement | null = null;
   private isDragging = false;
   private dragOffsetX = 0;
   private dragOffsetY = 0;
+  private pluginSettings: ObsidianChiikawaSettings = DEFAULT_SETTINGS;
 
   async onload() {
+    await this.loadSettings();
     this.showFloatingBox();
   }
 
@@ -27,6 +39,7 @@ export default class ObsidianChiikawaPlugin extends Plugin {
     });
 
     this.boxEl = boxEl;
+    this.applyBoxPosition();
 
     this.registerDomEvent(boxEl, "mousedown", (event) => {
       this.startDragging(event);
@@ -66,8 +79,47 @@ export default class ObsidianChiikawaPlugin extends Plugin {
     this.boxEl.style.top = `${nextY}px`;
   }
 
-  private stopDragging() {
+  private async stopDragging() {
+    const shouldSavePosition = this.isDragging && this.boxEl;
+
     this.isDragging = false;
     this.boxEl?.removeClass("is-dragging");
+
+    if (shouldSavePosition) {
+      await this.saveBoxPosition();
+    }
+  }
+
+  private applyBoxPosition() {
+    if (!this.boxEl) {
+      return;
+    }
+
+    this.boxEl.style.left = `${this.pluginSettings.boxX}px`;
+    this.boxEl.style.top = `${this.pluginSettings.boxY}px`;
+  }
+
+  private async saveBoxPosition() {
+    if (!this.boxEl) {
+      return;
+    }
+
+    const boxRect = this.boxEl.getBoundingClientRect();
+
+    this.pluginSettings.boxX = boxRect.left;
+    this.pluginSettings.boxY = boxRect.top;
+
+    await this.saveSettings();
+  }
+
+  private async loadSettings() {
+    this.pluginSettings = {
+      ...DEFAULT_SETTINGS,
+      ...(await this.loadData()),
+    };
+  }
+
+  private async saveSettings() {
+    await this.saveData(this.pluginSettings);
   }
 }
